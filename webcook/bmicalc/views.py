08bd3fd75e0268cb45cc8fd54ecb07747a18e69d
Cookie2222 <19522398@gm.uit.edu.vn>
2023-11-23 14:17:23 +0700

    Change a bit on BMI calc

--- webcook/bmicalc/views.py
+++ webcook/bmicalc/views.py
@@ -21,7 +21,7 @@ def BMI(request):
 
         if(age < 18):
             context = {
-                'message': "Sorry, you must be older than 18 years old to do BMI calculate",
+                'message_ok': "Sorry, you must be older than 18 years old to do BMI calculate",
                 'user': user
             }
             return HttpResponse(template.render(context))
@@ -30,28 +30,28 @@ def BMI(request):
             if(bmi < 18.5):
                 context = {
                     'result': bmi,
-                    'message': "It's seems like you are underweight, but don't worry, these articles provided down there can help you gain some weight",
+                    'message_not_ok': "It's seems like you are underweight, but don't worry, these articles provided down there can help you gain some weight",
                     'user': user
                 }
                 return HttpResponse(template.render(context))
             elif (bmi >= 18.5 and bmi <= 24.9):
                 context = {
                     'result': bmi,
-                    'message': "Your BMI indicates you are healthy, good job, keep up the work",
+                    'message_ok': "Your BMI indicates you are healthy, good job, keep up the work",
                     'user': user
                 }   
                 return HttpResponse(template.render(context))
             elif(bmi >= 25 and bmi <= 29.9):
                 context = {
                     'result': bmi,
-                    'message': "It's seems you are a bit overweight, losing some maybe the best idea, these articles provided down there can help you lose some weight",
+                    'message_not_ok': "It's seems you are a bit overweight, losing some maybe the best idea, these articles provided down there can help you lose some weight",
                     'user': user
                 }
                 return HttpResponse(template.render(context))
             else:
                 context = {
                     'result': bmi,
-                    'message': "You are, quite over the chart, these articles provided down there can help you lose some weight",
+                    'message_not_ok': "You are, quite over the chart, these articles provided down there can help you lose some weight",
                     'user': user
                 }
                 return HttpResponse(template.render(context))
