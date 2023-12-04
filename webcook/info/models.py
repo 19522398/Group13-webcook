81e83c094a4df3a5217a216a9693a390392eb021
Cookie2222 <19522398@gm.uit.edu.vn>
2023-12-04 15:31:52 +0700

    Some front-end change and new recipe

--- webcook/info/models.py
+++ webcook/info/models.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Create your models here.
 
 class WebcookUser(models.Model):
-    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
     gender = models.CharField(null=True)
     weight = models.CharField(max_length=4)
     height = models.CharField(max_length=3)
